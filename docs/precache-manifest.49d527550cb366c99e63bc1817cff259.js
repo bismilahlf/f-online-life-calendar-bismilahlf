b0ab7347286fea9fe3f90b12b40b016afaf7d804
bismilahlf <bismilah.lf@gmail.com>
2018-10-23 12:03:40 +0200

    Publish on github pages

---
+++ docs/precache-manifest.49d527550cb366c99e63bc1817cff259.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "c38681dd0f8a70e5d262",
+    "url": "./static/js/main.c38681dd.chunk.js"
+  },
+  {
+    "revision": "4fd91c74e51506835aa9",
+    "url": "./static/js/1.4fd91c74.chunk.js"
+  },
+  {
+    "revision": "c38681dd0f8a70e5d262",
+    "url": "./static/css/main.90c7355f.chunk.css"
+  },
+  {
+    "revision": "dc6a40d73e0f203adb91bc1c3a54052e",
+    "url": "./index.html"
+  }
+];
