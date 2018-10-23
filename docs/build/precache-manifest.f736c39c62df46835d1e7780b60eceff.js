df8dfa2860e388b742f68cd12148b764403e008a
bismilahlf <bismilah.lf@gmail.com>
2018-10-23 12:23:47 +0200

    Add changes to github pages

---
+++ docs/build/precache-manifest.f736c39c62df46835d1e7780b60eceff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "fa1581f83318f24f63a4",
+    "url": "./static/js/main.fa1581f8.chunk.js"
+  },
+  {
+    "revision": "4fd91c74e51506835aa9",
+    "url": "./static/js/1.4fd91c74.chunk.js"
+  },
+  {
+    "revision": "fa1581f83318f24f63a4",
+    "url": "./static/css/main.9dc7ee2c.chunk.css"
+  },
+  {
+    "revision": "527dea1fae39977a9526186e89abb5ac",
+    "url": "./index.html"
+  }
+];
